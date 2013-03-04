c7227b6b9e437241dd12b5600bff2bab866dcffa
Jaap Jolman <jaap@jolman.eu>
2013-03-04 10:20:45 +0100

    Added Dutch Language

---
+++ language/dutch/color_picker_lang.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+$lang['streams.color_picker.name']                              = 'Kleur kiezer';
+$lang['streams.color_picker.default_color']                     = 'Standaard kleur';
+$lang['streams.color_picker.options']                           = 'Invoer opties';
+$lang['streams.color_picker.disabled']                          = 'Invoer is uitgeschakeld';
+$lang['streams.color_picker.readonly']                          = 'Invoer is alleen-lezen';
+$lang['streams.color_picker.ishidden']                          = 'Invoer is verborgen, kleur kiezer is zichtbaar';
